3a4c40c9783602d1a5231a9607756a1de4f4c67b
Noufal <noufal24rahman@gmail.com>
2023-02-27 15:08:00 +0530

    yada yada

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,27 @@ import type { AppProps } from 'next/app'
 import { createTheme, ThemeProvider } from '@mui/material'
 const {myColors} = require("../../themeOveride.js")
 const theme = createTheme({
-  palette: myColors
+  palette: {
+    primary: {
+      main: '#f1b900',
+      light: "#bbb2b2"
+    }
+  }
 })
 import AppContext from '../contexts/app'
 import { useEffect, useState } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '../services/firebase'
+import { useRouter } from 'next/router'
 export default function App({ Component, pageProps }: AppProps) {
   const [session, setSession] = useState<any | null>({})
+  const router = useRouter()
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user != null) {
         setSession(user as any)
+      } else {
+        router.push('/login')
       }
     })
   })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,9 @@ import { auth } from '../services/firebase'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [data, setData] = useState<object>({})
-  const { session, setSession } = useContext(AppContext)
-  useEffect(() => {
-    setData(session)
-  })
-  const handleLogin = () => {
-    const provider = new GoogleAuthProvider()
-    provider.setCustomParameters({hd: "gct.ac.in"})
-    signInWithPopup(auth, provider).then(res => setSession(res.user))
-  }
   return (
-   <div>
-    {Object.keys(data).length==0 ? (<Button className="text-error-main" onClick={handleLogin}> Hello </Button>) : (<p>Logged in</p>)}
-   </div>
+    <div>
+      <h1>Hello World! This is the Home page</h1>
+    </div>
   )
 }
