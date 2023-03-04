e595faf0184c49ea2bd68d90f3a76445ab4fea32
Noufal <noufal24rahman@gmail.com>
2023-03-05 01:43:54 +0530

    fix: theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,22 @@ import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/react'
+import { Button } from '@mui/material'
 
 export default function Home() {
   const router = useRouter()
   const { data: session } = useSession()
-  useEffect(() => {
-    if (!session) {
-      router.push('/login')
-    } 
-  })
+  // useEffect(() => {
+  //   if (!session) {
+  //     router.push('/login')
+  //   } 
+  // })
+
   return (
     <div>
       <h1>Hello World! This is the Home page</h1>
+      <Button>lkergiop</Button>
+
     </div>
   )
 }
